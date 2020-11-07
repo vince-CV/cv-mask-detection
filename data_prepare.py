6b1357688b1f88ac9b02da4b7ca59df2f4ba7726
vince-CV <vince@arylla.com>
2020-11-07 17:40:42 -0500

    added a module for custom object detector

--- data_prepare.py
+++ data_prepare.py
@@ -8,8 +8,10 @@ import sys
 
 image_dir = "C:/Users/xwen2/Google Drive/Mask Detector/data"
 image_dir_save = "/content/drive/My Drive/Mask Detector"
-f_val = open("C:/Users/xwen2/Google Drive/Mask Detector/data_test.txt", 'w')
-f_train = open("C:/Users/xwen2/Google Drive/Mask Detector/data_train.txt", 'w')
+
+# Absolute path of output .txt files
+f_val = open("C:/Users/xwen2/Google Drive/Mask Detector/data_test_rp1.txt", 'w')
+f_train = open("C:/Users/xwen2/Google Drive/Mask Detector/data_train_rp1.txt", 'w')
 
 path, dirs, files = next(os.walk(image_dir))
 data_size = 0.5 * len(files)
